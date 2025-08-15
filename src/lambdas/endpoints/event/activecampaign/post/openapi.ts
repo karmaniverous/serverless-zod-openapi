e3f92b23a734113fa8d115cdaee25203d1a766ae
Jason Williscroft <me@karmanivero.us>
2025-08-15 20:45:55 +0800

    wip

--- src/lambdas/endpoints/event/activecampaign/post/openapi.ts
+++ src/lambdas/endpoints/event/activecampaign/post/openapi.ts
@@ -3,26 +3,31 @@ import { buildPathItemObject } from '@@/src/openapi/buildPathItemObject';
 import { eventSchema, responseSchema } from './schema';
 import { securityContexts } from './securityContexts';
 
-export default buildPathItemObject(securityContexts, 'event/activecampaign', 'post', {
-  summary: 'ActiveCampaign events',
-  description: 'Receive ActiveCampaign event webhooks.',
-  requestBody: {
-    description: 'Webhook payload.',
-    content: {
-      'application/json': {
-        schema: eventSchema.shape.body,
-      },
-    },
-  },
-  responses: {
-    200: {
-      description: 'Ok',
+export default buildPathItemObject(
+  securityContexts,
+  'event/activecampaign',
+  'post',
+  {
+    summary: 'ActiveCampaign events',
+    description: 'Receive ActiveCampaign event webhooks.',
+    requestBody: {
+      description: 'Webhook payload.',
       content: {
         'application/json': {
-          schema: responseSchema,
+          schema: eventSchema.shape.body,
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Ok',
+        content: {
+          'application/json': {
+            schema: responseSchema,
+          },
+        },
+      },
+    },
+    tags: [],
   },
-  tags: [],
-});
+);
