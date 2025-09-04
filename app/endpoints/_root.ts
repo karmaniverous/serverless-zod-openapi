728b2c858b7cf3a6c3dcb4db405bd2c2da014b96
Jason Williscroft <me@karmanivero.us>
2025-09-04 12:38:15 +0800

    reorganized app directory

--- app/endpoints/_root.ts
+++
@@ -1,5 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-/** Absolute path to the endpoints root folder. */
-export const ENDPOINTS_ROOT = dirname(fileURLToPath(import.meta.url));
